fd0d58da543688da4ec4a1c27c3ad686c847a934
Dmitry Morozov <dvmorozov@hotmail.com>
2019-05-17 09:17:44 +0200

    IInjectorConfig and base implementation have been added.
    #3

---
+++ HTML cleanup/HTMLCleanup/BaseInjectorConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace HTMLCleanup
+{
+    class BaseInjectorConfig : IInjectorConfig
+    {
+        public List<HtmlCleanerConfigItem> GetCleanerList()
+        {
+            return new List<HtmlCleanerConfigItem>() {
+                new HtmlCleanerConfigItem() { urlPrefix = "https://rationalcity.wordpress.com/", htmlCleanerType = "WordPressHtmlCleaner" },
+                new HtmlCleanerConfigItem() { urlPrefix = "", htmlCleanerType = "UniversalHtmlCleaner" }
+            };
+        }
+    }
+}
---
+++ HTML cleanup/HTMLCleanup/IntInjectorConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace HTMLCleanup
+{
+    public class HtmlCleanerConfigItem
+    {
+        public string urlPrefix;
+        public string htmlCleanerType;
+    }
+
+    interface IInjectorConfig
+    {
+        /// <summary>
+        /// Returns list of cleaner type names.
+        /// </summary>
+        /// <returns>List of cleaner type names.</returns>
+        List<HtmlCleanerConfigItem> GetCleanerList();
+    }
+}
