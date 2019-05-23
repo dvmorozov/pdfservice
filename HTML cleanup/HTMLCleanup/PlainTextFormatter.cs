2d12ab9795146c9bbca6181863ea2bff3504d775
Dmitry Morozov <dvmorozov@hotmail.com>
2019-05-23 09:32:03 +0200

    Simple plain text formatter has been implemented.
    Closes #17

--- HTML cleanup/HTMLCleanup/PlainTextFormatter.cs
+++ HTML cleanup/HTMLCleanup/PlainTextFormatter.cs
@@ -8,6 +8,20 @@
 
         public string Process(BaseHtmlCleaner.Tag tag, string innerText)
         {
+            switch (tag.StartTag)
+            {
+            case ("<ul"):
+                //  Adds line break (line should not contain only white
+                //  spaces, otherwise it will be removed at next stages).
+                return "\\\n" + innerText + "\\\n";
+
+            case ("<li"):
+                return "  * " + innerText;
+
+            case ("<pre"):
+                var indent = "\\  ";
+                return indent + innerText.Replace("\n", "\n" + indent);
+            }
             return innerText;
         }
     }
