79166ed8a0d00fe7c8e45115246348b6f58160e6
Dmitry Morozov <dvmorozov@hotmail.com>
2019-05-24 09:00:15 +0200

    Prototype of PdfFormatter class was added.
    #4

---
+++ HTML cleanup/HTMLCleanup/PdfFormatter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HtmlCleanup
+{
+    class PdfFormatter : ITagFormatter
+    {
+        public string Process(BaseHtmlCleaner.Tag tag, string innerText)
+        {
+            switch (tag.StartTag)
+            {
+                case ("<ul"):
+                    return innerText;
+
+                case ("<li"):
+                    return innerText;
+
+                case ("<pre"):
+                    return innerText;
+            }
+            return innerText;
+        }
+    }
+}
