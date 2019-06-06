7f86f430b3ef48d0dacbe6cf35ef9914ec93c15c
Dmitry Morozov <dvmorozov@hotmail.com>
2019-06-06 09:33:51 +0200

    Code writing response to client was moved to separate function.
    #2

--- PdfService/Controllers/PdfController.cs
+++ PdfService/Controllers/PdfController.cs
@@ -49,6 +49,22 @@ namespace EnterpriseServices.Controllers
             return fileName;
         }
 
+        /// <summary>
+        /// Writes response data.
+        /// </summary>
+        /// <param name="dataStream">File data.</param>
+        /// <param name="contentType">MIME content type.</param>
+        /// <param name="fileName">Full file name including extension.</param>
+        private void SendFileResponse(MemoryStream dataStream, string contentType, string fileName)
+        {
+            Response.Clear();
+            Response.ContentType = contentType;
+            Response.AddHeader("Content-Disposition", "inline;filename=" + fileName);
+            Response.BinaryWrite(dataStream.ToArray());
+            Response.Flush();
+            Response.End();
+        }
+
         [AllowAnonymous]
         public ActionResult UrlToPdf(string url)
         {
@@ -74,13 +90,7 @@ namespace EnterpriseServices.Controllers
                     if (dataStream != null)
                     {
                         dataStream.Seek(0, SeekOrigin.Begin);
-
-                        Response.Clear();
-                        Response.ContentType = "application/pdf";
-                        Response.AddHeader("Content-Disposition", "inline;filename=" + UrlToFileName(url, ".pdf"));
-                        Response.BinaryWrite(dataStream.ToArray());
-                        Response.Flush();
-                        Response.End();
+                        SendFileResponse(dataStream, "application/pdf", UrlToFileName(url, ".pdf"));
                         return new EmptyResult();
                     }
                 }
