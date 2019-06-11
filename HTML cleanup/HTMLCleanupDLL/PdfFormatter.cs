23ce92769bb6ba39643fabb7c308ec2182cd5a3d
Dmitry Morozov <dvmorozov@hotmail.com>
2019-06-11 12:26:22 +0200

    Default font size was introduced.
    #24

--- HTML cleanup/HTMLCleanupDLL/PdfFormatter.cs
+++ HTML cleanup/HTMLCleanupDLL/PdfFormatter.cs
@@ -20,6 +20,7 @@ namespace HtmlCleanup
         private bool _preformatted;     //  Add "preformatted" style.
         private PdfWriter _writer;
         private float _defaultPadding = 10;
+        private float _defaultFontSize = 14;
 
         public MemoryStream GetOutputStream()
         {
@@ -57,7 +58,8 @@ namespace HtmlCleanup
                     _list = new List()
                         .SetSymbolIndent(12)
                         .SetListSymbol("\u2022")
-                        .SetFont(_font);
+                        .SetFont(_font)
+                        .SetFontSize(_defaultFontSize);
                     callFinalize = true;
                     break;
 
@@ -111,11 +113,13 @@ namespace HtmlCleanup
             {
                 var paragraph = new Paragraph().SetFont(_font);
                 paragraph.Add(finalText);
+                paragraph.SetFontSize(_defaultFontSize);
 
                 if (_preformatted)
                 {
                     paragraph.SetBackgroundColor(ColorConstants.LIGHT_GRAY);
                     paragraph.SetFontFamily(new string[] { iText.IO.Font.Constants.StandardFonts.COURIER });
+                    paragraph.SetFontSize(10);
                     paragraph.SetPadding(_defaultPadding);
                     paragraph.SetBorder(new iText.Layout.Borders.SolidBorder(ColorConstants.GRAY, 1));
                     _preformatted = false;
