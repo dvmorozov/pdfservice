21ead2f5d28706c5869fbf1d27e94a7e2b37273d
Dmitry Morozov <dvmorozov@hotmail.com>
2020-10-02 16:14:01 +0200

    Conversion by PdfCreator moved to separate class.
    #51

---
+++ PdfService/HtmlToPdf/HtmlToPdfByPdfCreator.cs
@@ -0,0 +1,42 @@
+﻿using EnterpriseServices.Controllers;
+using System;
+using System.Diagnostics;
+
+namespace EnterpriseServices.HtmlToPdf
+{
+    public class HtmlToPdfByPdfCreator
+    {
+        private readonly PdfController _pdfController;
+
+        public HtmlToPdfByPdfCreator(PdfController pdfController)
+        {
+            _pdfController = pdfController;
+        }
+
+        public string GetUrlToPdf(string url)
+        {
+            string pdfFileName = _pdfController.UrlToFileName(url);
+            string converterPath = _pdfController.Server.MapPath("~") + "PdfCreator\\create_pdf_from_html.bat";
+            string pdfFilePath = _pdfController.Server.MapPath("~") + "Content\\" + pdfFileName;
+            string arguments = url + " " + pdfFilePath;
+
+            ProcessStartInfo startInfo = new ProcessStartInfo(converterPath, arguments)
+            {
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                WorkingDirectory = _pdfController.Server.MapPath("~") + "PdfCreator\\"
+            };
+
+            Process process = Process.Start(startInfo);
+            process.WaitForExit();
+
+            UriBuilder urlBuilder = new UriBuilder(_pdfController.Request.Url.AbsoluteUri)
+            {
+                Path = _pdfController.Url.Content("~/Content/" + pdfFileName),
+                Query = null,
+            };
+
+            return urlBuilder.ToString();
+        }
+    }
+}
