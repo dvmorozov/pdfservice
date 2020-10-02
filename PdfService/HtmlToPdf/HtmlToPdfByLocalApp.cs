b729c8ac20ebfc7254b5ecbd33d12049ff53c24e
Dmitry Morozov <dvmorozov@hotmail.com>
2020-10-03 00:07:11 +0200

    HtmlToPdfByPdfCreator generalized to HtmlToPdfByLocalApp.
    #51

--- PdfService/HtmlToPdf/HtmlToPdfByLocalApp.cs
+++ PdfService/HtmlToPdf/HtmlToPdfByLocalApp.cs
@@ -4,19 +4,21 @@ using System.Diagnostics;
 
 namespace EnterpriseServices.HtmlToPdf
 {
-    public class HtmlToPdfByPdfCreator
+    public class HtmlToPdfByLocalApp
     {
         private readonly PdfController _pdfController;
+        private readonly string _localAppDirectory;
 
-        public HtmlToPdfByPdfCreator(PdfController pdfController)
+        public HtmlToPdfByLocalApp(PdfController pdfController, string localAppDirectory)
         {
             _pdfController = pdfController;
+            _localAppDirectory = localAppDirectory;
         }
 
         public string GetUrlToPdf(string url)
         {
             string pdfFileName = _pdfController.UrlToFileName(url);
-            string converterPath = _pdfController.Server.MapPath("~") + "PdfCreator\\create_pdf_from_html.bat";
+            string converterPath = _pdfController.Server.MapPath("~") + _localAppDirectory + "\\create_pdf_from_html.bat";
             string pdfFilePath = _pdfController.Server.MapPath("~") + "Content\\" + pdfFileName;
             string arguments = url + " " + pdfFilePath;
 
@@ -24,7 +26,7 @@ namespace EnterpriseServices.HtmlToPdf
             {
                 UseShellExecute = false,
                 CreateNoWindow = true,
-                WorkingDirectory = _pdfController.Server.MapPath("~") + "PdfCreator\\"
+                WorkingDirectory = _pdfController.Server.MapPath("~") + _localAppDirectory + "\\"
             };
 
             Process process = Process.Start(startInfo);
